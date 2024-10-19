d23c44b5fbe8fd22119821cc94979f8795df3aed
Ubai Ratangiri <112539947+ubaicoder@users.noreply.github.com>
2024-10-19 19:41:25 +0530

    Update and rename Main.java to Test.java

--- Main.java
+++
@@ -1,7 +0,0 @@
-public class Main{
-    public static void main(String[]args){
-      // int a = 5;
-      // int b = 54;
-      System.out.println("Addition of "+args[0]+" & "+args[1]+" is "+ (Integer.parseInt(args[0])+Integer.parseInt(args[1])));
-    }
-}
---
+++ Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args) {
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
+        System.out.println("Addition of " + num1 + " & " + num2 + " is " + (num1 + num2));
+    }
+}
